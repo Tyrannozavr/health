7ab1d5805579635c42a3f6d5889ad604f6697327
dmiv <dmitriy40647274@gmail.com>
2023-04-20 01:27:12 +0300

    чуть подправил

--- accounts/views.py
+++ accounts/views.py
@@ -6,29 +6,18 @@ def register(request):
     User = get_user_model()
     if request.POST:
         email = request.POST.get('email')
+        if User.objects.get(email=email):
+            print('exist')
+            context = {'message': 'already exist'}
+            return render(request, 'accounts/register.html', context=context)
         password = ''
         for x in range(10):  # Количество символов (16)
             password = password + random.choice(list('1234567890abcdefghigklmnopqrstuvyxwzABCDEFGHIGKLMNOPQRSTUVYXWZ'))
         user = User.objects.create_user(email)
         user.set_password(password)
-        context = {'email': email, 'password': password, 'user': user}
+        context = {'email': email, 'password': password}
     else:
         context = {}
     return render(request, 'accounts/register.html', context=context)
 
 
-
-# from django.contrib.auth import authenticate, login
-#
-#
-# def my_view(request):
-#     username = request.POST["username"]
-#     password = request.POST["password"]
-#     user = authenticate(request, username=username, password=password)
-#     if user is not None:
-#         login(request, user)
-#         # Redirect to a success page.
-#         ...
-#     else:
-#         # Return an 'invalid login' error message.
-#         ...
