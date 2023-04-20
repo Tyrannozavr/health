82a93275a0b42cdb342b774c9008d63b95a926c2
dmiv <dmitriy40647274@gmail.com>
2023-04-20 19:20:35 +0300

    login

--- accounts/views.py
+++ accounts/views.py
@@ -1,19 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model
+from django.contrib.auth import authenticate, login
 
 import random
 def register(request):
     User = get_user_model()
     if request.POST:
         email = request.POST.get('email')
-        # print(email)
         if User.objects.filter(email=email).exists():
             context = {'message': 'already exist'}
             return render(request, 'accounts/register.html', context=context)
         password = ''
         for x in range(10):  # Количество символов (16)
             password = password + random.choice(list('1234567890abcdefghigklmnopqrstuvyxwzABCDEFGHIGKLMNOPQRSTUVYXWZ'))
-        # print(password, 'pass')
         user = User.objects.create_user(email)
         user.set_password(password)
         context = {'email': email, 'password': password}
@@ -22,3 +21,12 @@ def register(request):
     return render(request, 'accounts/register.html', context=context)
 
 
+def login(request):
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return redirect('/')
+    else:
+        return redirect('accounts/login')
