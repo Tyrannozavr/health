b58c4e1f2d2125817db95cb27e14ce65a5bd97ee
dmiv <dmitriy40647274@gmail.com>
2023-04-20 18:19:53 +0300

    изменил проверку

--- accounts/views.py
+++ accounts/views.py
@@ -6,13 +6,14 @@ def register(request):
     User = get_user_model()
     if request.POST:
         email = request.POST.get('email')
-        if User.objects.get(email=email):
-            print('exist')
+        # print(email)
+        if User.objects.filter(email=email).exists():
             context = {'message': 'already exist'}
             return render(request, 'accounts/register.html', context=context)
         password = ''
         for x in range(10):  # Количество символов (16)
             password = password + random.choice(list('1234567890abcdefghigklmnopqrstuvyxwzABCDEFGHIGKLMNOPQRSTUVYXWZ'))
+        # print(password, 'pass')
         user = User.objects.create_user(email)
         user.set_password(password)
         context = {'email': email, 'password': password}
