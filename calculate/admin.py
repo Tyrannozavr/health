be27213ef9353867f87dc0ef90297d93c1093f8a
dmiv <dmitriy40647274@gmail.com>
2023-04-20 06:32:20 +0300

    зарегал в админке все

--- calculate/admin.py
+++ calculate/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Trainings, Train, Exercise, Advices, Muscules, Dishes, Tracker
 
-# Register your models here.
+@admin.register(Trainings)
+class TrainingsAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Train)
+class TrainAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Exercise)
+class ExerciseAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Advices)
+class AdvicesAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Muscules)
+class MusculesAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Dishes)
+class DishesAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Tracker)
+class TrackerAdmin(admin.ModelAdmin):
+    pass
