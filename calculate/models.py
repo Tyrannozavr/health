452ba89f1272070815f196974019a9f66051d795
dmiv <dmitriy40647274@gmail.com>
2023-04-20 06:08:08 +0300

    модельки для тренировки, корявые, но рабочие

--- calculate/models.py
+++ calculate/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Trainings(models.Model):
+    h1 = models.CharField(max_length=40)
+    slug = models.SlugField()
+    description = models.CharField(max_length=50)
+
+class Train(models.Model):
+    training = models.ForeignKey(Trainings, on_delete=models.CASCADE)
+    h2 = models.CharField(max_length=40)
+    content = models.CharField(max_length=200)
+
+    # например trainings h1 = день первый slug это к примеру ссылка, можешь заменить просто на charfield
+    # дальше, h2 разминка, description = стройность пантеры, content сама тренировка.
+    # ниже практические советы и реки они тоже записывааются в train и через fk связывааются с треней, в общем сколько
+    # нужно, столько на одной странице и можно добавить
